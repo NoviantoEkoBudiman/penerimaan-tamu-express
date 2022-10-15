83a3361263ddfdcede95b68f03fea8100af0a29e
NoviantoEkoBudiman <stardustvian@gmail.com>
2022-10-15 12:32:54 +0700

    deploy: add admin module

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var scheduleRouter = require('./routes/schedule');
 var reservationRouter = require('./routes/reservation');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -32,5 +33,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reservation', reservationRouter);
 app.use('/schedule', scheduleRouter);
+app.use('/admin', adminRouter);
 
 module.exports = app;
--- routes/schedule.js
+++ routes/schedule.js
@@ -4,15 +4,22 @@ const { Reservation } = require('../models');
 
 router.get('/', async(req, res)=>{
     const page = 'schedule';
+    let scheldule = [];
+
     const data = await Reservation.findAll({
         where:{
             reservation_status: 'disetujui'
         }
     });
-    res.json(data);
-    // res.render("../public/schedule/index",{
-    //     page: page
-    // });
+
+    // for(let i=0;i<data.length;i++){
+    //     console.log(data[0]);
+    // }
+    // res.send(data);
+    res.render("../public/schedule/index",{
+        page: page,
+        data: data
+    });
 });
 
 module.exports = router;
